6c32b4d7af8b164be2b2c2bfa62c9ddda31afe50
Filip Hesse <Filip_Hesse@yahoo.de>
2020-07-21 12:43:57 +0200

    Make python package executable

--- ros2_ws/src/tutorial_py/setup.py
+++ ros2_ws/src/tutorial_py/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+                'talker_node = tutorial_py.talker:main',
+                'listener_node = tutorial_py.listener:main',
         ],
     },
 )
