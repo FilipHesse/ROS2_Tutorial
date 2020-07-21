e80c75584e856dfd40cc8b3482a98fd0bb505b9e
Filip Hesse <Filip_Hesse@yahoo.de>
2020-07-21 10:07:08 +0200

    Add python versions of Packages including listeners and talkers

---
+++ ros2_ws/src/tutorial_py/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'tutorial_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='filip',
+    maintainer_email='Filip_Hesse@yahoo.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
