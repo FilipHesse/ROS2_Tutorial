8ffdcaacbe904ed40e14f39253b9197c14ef5eb9
imstevenpm.study <imstevenpm.study@gmail.com>
2020-07-29 17:20:02 +0200

    Changed file structured and added action in cpp

--- ros2_ws/src/action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
+++ ros2_ws/src/action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
@@ -1,17 +1,36 @@
+#!/usr/bin/env python3
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.import time
+
 import time
+
+from action_tutorials_interfaces.action import Fibonacci
 
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
-
-from action_tutorials.action import Fibonacci
 
 
 class FibonacciActionServer(Node):
 
     def __init__(self):
         super().__init__('fibonacci_action_server')
-        self._action_server = ActionServer(self,Fibonacci,'fibonacci',self.execute_callback)
+        self._action_server = ActionServer(
+            self,
+            Fibonacci,
+            'fibonacci',
+            self.execute_callback)
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
@@ -31,13 +50,17 @@ class FibonacciActionServer(Node):
         result = Fibonacci.Result()
         result.sequence = feedback_msg.partial_sequence
         return result
-        
+
+
 def main(args=None):
     rclpy.init(args=args)
 
     fibonacci_action_server = FibonacciActionServer()
 
-    rclpy.spin(fibonacci_action_server)
+    try:
+        rclpy.spin(fibonacci_action_server)
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
