1db4e6504893f1ff2bca0ff9bf64e0ca16589e34
danieto98 <danieto98@gmail.com>
2020-07-30 11:41:53 +0200

    Renamed tutorial_interfaces to tutorial_msgs and deleted unnecessary dependencies

--- ros2_ws/src/tutorial_cpp/src/ros_bridge.cpp
+++ ros2_ws/src/tutorial_cpp/src/ros_bridge.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "tutorial_interfaces/msg/custom_message.hpp"
+#include "tutorial_msgs/msg/custom_message.hpp"
 using std::placeholders::_1;
 
 class ROS1Subscriber : public rclcpp::Node
@@ -10,16 +10,16 @@ class ROS1Subscriber : public rclcpp::Node
     ROS1Subscriber()
     : Node("ros_bridge_2")
     {
-      subscription_ = this->create_subscription<tutorial_interfaces::msg::CustomMessage>(
+      subscription_ = this->create_subscription<tutorial_msgs::msg::CustomMessage>(
       "topic", 10, std::bind(&ROS1Subscriber::class_callback, this, _1));
     }
 
   private:
-    void class_callback(const tutorial_interfaces::msg::CustomMessage::SharedPtr msg) const
+    void class_callback(const tutorial_msgs::msg::CustomMessage::SharedPtr msg) const
     {
       RCLCPP_INFO(this->get_logger(), "Class detected: '%s'", msg->class_name.c_str());
     }
-    rclcpp::Subscription<tutorial_interfaces::msg::CustomMessage>::SharedPtr subscription_;
+    rclcpp::Subscription<tutorial_msgs::msg::CustomMessage>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
